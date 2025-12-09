03e355e16e3bbfa060c3349ec20a93e70e6867d9
HarshitTiwari-20 <ht337378@gmail.com>
2025-12-09 11:02:07 +0530

    changes

--- longest_consecutive_seq.cpp
+++ longest_consecutive_seq.cpp
@@ -13,11 +13,12 @@ class Solution {
       for(auto it : st){
         if(st.find(it - 1) == st.end()){
           int cnt = 1;
+          int x = it;
           while (st.find(x+1) != st.end()) {
             x = x + 1;
             cnt = cnt + 1;
 
-            
+        
           }
           longest = max(longest, cnt);
         }
