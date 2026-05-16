5497b1f89e799f950d434c9eaca3e35ee4a55867
HarshitTiwari-20 <ht337378@gmail.com>
2026-05-17 00:06:47 +0530

    3sum

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,3 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        if(i>0 && nums[i] == nums[i-1]) continue;
+        int j = i +1;
+        int k = n -1;
+        while(j<k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0){
+                j++;
+            }
+            else if(sum>0){
+                k--; 
+            }
+            else {
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j < k && nums[j] == nums[j-1]) j++;
+                while(j < k && nums[k] == nums[k+1]) k--;
+                
+            }
+        }
+    }
+    return ans;
+
+}
+        
+    
+};
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -28,6 +69,120 @@ public:
 
         // Convert set to vector
         vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
+
+// Driver code
+int main() {
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    Solution obj;
+    vector<vector<int>> res = obj.threeSum(arr, n);
+
+    for (auto &triplet : res) {
+        for (auto &num : triplet) cout << num << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//================================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Class to solve 3-sum problem
+class Solution {
+public:
+    // Function to find triplets with sum zero
+    vector<vector<int>> threeSum(vector<int>& arr, int n) {
+        // Store unique triplets
+        set<vector<int>> ans;
+
+        // First loop for first element
+        for (int i = 0; i < n; i++) {
+            // Set to store elements seen in this iteration
+            set<int> hashset;
+
+            // Second loop for second element
+            for (int j = i + 1; j < n; j++) {
+                // Calculate third element needed
+                int third = -(arr[i] + arr[j]);
+
+                // If third already in set, we found a triplet
+                if (hashset.find(third) != hashset.end()) {
+                    vector<int> temp = {arr[i], arr[j], third};
+                    sort(temp.begin(), temp.end());
+                    ans.insert(temp);
+                }
+
+                // Add current element to set
+                hashset.insert(arr[j]);
+            }
+        }
+
+        // Convert set to vector
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
+
+// Driver code
+int main() {
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    Solution obj;
+    vector<vector<int>> res = obj.threeSum(arr, n);
+
+    for (auto &triplet : res) {
+        for (auto &num : triplet) cout << num << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//===============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Class to solve 3-sum problem
+class Solution {
+public:
+    // Function to find triplets with sum zero
+    vector<vector<int>> threeSum(vector<int>& arr, int n) {
+        // Sort the array
+        sort(arr.begin(), arr.end());
+        // Store final result
+        vector<vector<int>> ans;
+
+        // First loop for first element
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates for first element
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            // Two pointers
+            int left = i + 1, right = n - 1;
+
+            // Find pairs for current arr[i]
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    ans.push_back({arr[i], arr[left], arr[right]});
+                    left++, right--;
+
+                    // Skip duplicates for left
+                    while (left < right && arr[left] == arr[left - 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && arr[right] == arr[right + 1]) right--;
+                }
+                else if (sum < 0) left++;
+                else right--;
+            }
+        }
         return ans;
     }
 };
